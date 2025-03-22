b7e7211aee7aeecc8d22940a129b734612805d0c
James Rhamy <1060896+Akidi@users.noreply.github.com>
2025-03-22 06:08:41 -0500

    feat(users): add users table and display in login/register page

--- app/src/routes/demo/lucia/login/+page.svelte
+++ app/src/routes/demo/lucia/login/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import type { ActionData } from './$types';
+	import type { ActionData, PageServerData } from './$types';
 
-	let { form }: { form: ActionData } = $props();
+	let { form, data }: { form: ActionData, data?: PageServerData } = $props();
+
+	let users = $derived(data?.users || []);
 </script>
 
 <h1>Login/Register</h1>
@@ -19,3 +21,23 @@
 	<button formaction="?/register">Register</button>
 </form>
 <p style="color: red">{form?.message ?? ''}</p>
+
+
+<table>
+	<thead>
+		<tr>
+			<th>Username</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each users as user}
+			<tr>
+				<td>{user.username}</td>
+			</tr>
+		{:else}
+			<tr>
+				<td>No users found</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
